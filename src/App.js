e7d4176316b6a3fa57bad5e114973d14bd550076
Arrastia <arrastia@pm.me>
2021-04-09 09:19:03 +0200

    add env

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const { REACT_APP_API_KEY, REACT_APP_TEST } = process.env;
+
+  console.log(`REACT_APP_TEST`, REACT_APP_TEST);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit{" "}
+          <code>
+            {REACT_APP_API_KEY.length === 8 ? "src/App.js" : "github"}{" "}
+          </code>{" "}
+          and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
